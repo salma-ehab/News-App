1e483e2f3c8a91f784bf4bd4befd49bb26bd73c1
salma-ehab <salma.ehabg@gmail.com>
2020-09-20 15:25:08 -0700

    model and network:added, HeadlinesFragment:modified.

---
+++ app/src/main/java/com/example/newsappinkotlin/ui/model/NewsModel.kt
@@ -0,0 +1,14 @@
+package com.example.newsappinkotlin.ui.model
+
+import com.google.gson.annotations.SerializedName
+
+data class NewsModel (
+    @SerializedName("source") val source:SourceModel,
+    @SerializedName("author") var author:String,
+    @SerializedName("title") var title:String,
+    @SerializedName("description") var desciption:String,
+    @SerializedName("url") var url:String,
+    @SerializedName("urlToImage") var urlToImage:String,
+    @SerializedName("publishedAt") var publishedAt:String,
+    @SerializedName("content") var content:String
+)
