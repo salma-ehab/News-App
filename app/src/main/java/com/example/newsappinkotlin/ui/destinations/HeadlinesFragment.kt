8abfb0d80fc0c9d823ced6453054a8e16a227e69
MaryamMoh <71566669+MaryamMoh@users.noreply.github.com>
2020-09-22 22:42:26 +0200

    on scroll listener, implementation of onsuccess and onerror alis functions

--- app/src/main/java/com/example/newsappinkotlin/ui/destinations/HeadlinesFragment.kt
+++ app/src/main/java/com/example/newsappinkotlin/ui/destinations/HeadlinesFragment.kt
@@ -7,34 +7,65 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.newsappinkotlin.HeadlinesAdapter
 import com.example.newsappinkotlin.R
 import com.example.newsappinkotlin.model.NewsModel
+import com.example.newsappinkotlin.network.APIServices
+import com.example.newsappinkotlin.ui.destinations.HeadlinesFragment
+
 import com.example.newsappinkotlin.network.NewsClient
+import kotlinx.android.synthetic.main.fragment_headlines.*
 
 class HeadlinesFragment : Fragment() {
-    var currentPage=1
+    var currentPage = 1
+    lateinit var Adapter: HeadlinesAdapter
+    lateinit var llm: LinearLayoutManager
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.fragment_headlines, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-       getNews()
-    }
-    fun getNews()
-    {
-        Log.d("AllNews","here")
-        NewsClient.fetchHeadlines(currentPage,::onSuccess,::onError)
-    }
-// These functions are to be modified on adding the view model
-    private fun onError()
-    {
-        Toast.makeText(getActivity(),"Failed to get news", Toast.LENGTH_SHORT).show()
+
+
+        Adapter = HeadlinesAdapter(mutableListOf(), this)
+        llm = LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, false)
+
+        rv_news.adapter = Adapter
+        rv_news.layoutManager = llm
+        getNews()
     }
 
-    private fun onSuccess(list: MutableList<NewsModel>)
-    {
-       //waiting  to append data from adapter
+    fun getNews() {
+        Log.d("AllNews", "here")
+        NewsClient.fetchHeadlines(currentPage, ::onSuccess, ::onError)
+    }
+
+    // These functions are to be modified on adding the view model
+    fun onError() {
+        Toast.makeText(getActivity(), "Failed to get news", Toast.LENGTH_SHORT).show()
+    }
+
+    fun onSuccess(list: MutableList<NewsModel>) {
+        Adapter.appendMovies(list)
+
+        rv_news.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                val totalItems = llm.itemCount
+                val visibleItemsCount = llm.childCount
+                val firstVisibleItem = llm.findLastVisibleItemPosition()
+
+                if (firstVisibleItem + visibleItemsCount >= totalItems / 2) {
+                    rv_news.removeOnScrollListener(this)
+                    currentPage++
+                    getNews()
+                }
+            }
+        })
+
     }
 }
+
 
